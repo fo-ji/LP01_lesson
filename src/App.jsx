4307d78fd082fe8064862aeaf43b81eaa28dc349
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-25 10:14:40 +0900

    開発環境の構築

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./assets/reset.css"
+import "./assets/style.scss"
+
+const App = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <main>
+        {/* <Home /> */}
+        Hello, World!
+      </main>
+    </>
+  );
+}
+
+export default App;
