f05c751be35cbdf78316a5a9a9b11da189df221c
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-26 22:15:39 +0900

    Headerコンポーネントの実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import "./assets/reset.css"
 import "./assets/style.scss"
 import Home from "./templates/Home"
+import Header from "./templates/Header"
 
 const App = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <main>
         <Home />
       </main>
