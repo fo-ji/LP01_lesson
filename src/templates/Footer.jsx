b3fa9b3537c1191f1c2bc5ac6cb895e054667f51
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-11-08 17:52:06 +0900

    レスポンシブ対応の実装

--- src/templates/Footer.jsx
+++ src/templates/Footer.jsx
@@ -3,6 +3,8 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="footer">
+      <a href="https://www.careertrek.com/login"  className="pc-hide-text--login">ログイン</a>
+      <a href="https://bizreach.biz/service/careertrek/?trcd=careertrekC"  className="pc-hide-text--info">採用ご担当者の方はこちら</a>
       <div className="footer-image"></div>
       <div className="footer-contents">
         <p>Copyright (C) 2020 BizReach, Inc.</p>
--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ImageHeaderLogo from "../assets/img/header-image.svg"
+import ImageHeaderLogoPC from "../assets/img/header-image.svg"
+import ImageHeaderLogoSM from "../assets/img/header_logo_sm.svg"
 
 const Header = () => {
   function scrollFunction() {
@@ -21,10 +22,11 @@ const Header = () => {
   return (
     <div className="header" id="scroll">
       <div className="header-contents">
-        <img src={ImageHeaderLogo} alt="キャリトレ"></img>
+        <img src={ImageHeaderLogoPC} className="sm-hide" alt="キャリトレ"></img>
+        <img src={ImageHeaderLogoSM} className="pc-hide" alt="キャリトレ"></img>
         <nav>
           <ul>
-            <li>
+            <li className="sm-hide">
               <a href="https://bizreach.biz/service/careertrek/?trcd=careertrekC">採用ご担当者の方</a>
             </li>
             <li>
@@ -33,6 +35,9 @@ const Header = () => {
             <li id="fadein-list">
               <a href="https://www.careertrek.com/register/email">使ってみる</a>
             </li>
+            <li id="pc-hide">
+              <a href="https://www.careertrek.com/login">使ってみる</a>
+            </li> 
           </ul>
         </nav>
       </div>
